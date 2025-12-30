46fb19c145859b2d3056c1097ff14db376ee3ac3
sametcn99 <sametcn99@gmail.com>
2025-12-30 22:21:25 +0300

    .

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,11 +11,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			changeFrequency: "weekly",
 			priority: 1,
 		},
-		{
-			url: `${baseUrl}/page`,
-			lastModified: new Date(),
-			changeFrequency: "weekly",
-			priority: 0.8,
-		},
 	];
 }
